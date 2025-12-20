cc60bab50fb8b1b8f4921f369b571402e4413b2e
Lychee <2006tka@gmail.com>
2025-12-20 19:47:22 +0500

    coef 0.2 -> 0.5

--- Api/Application/Features/Tasks/ChangeTask/ChangeTaskHandler.cs
+++ Api/Application/Features/Tasks/ChangeTask/ChangeTaskHandler.cs
@@ -8,7 +8,7 @@ namespace Api.Application.Features.Tasks.ChangeTask;
 public class ChangeTaskHandler(IUsersTasksTable tasks, IUserTable users, IMetricsTable metrics, ITaskEvent taskEvent)
     : IRequestHandler<ChangeTaskQuery, TaskModel>
 {
-    private const double Coefficient = 0.2;
+    private const double Coefficient = 0.5;
 
     public async Task<TaskModel> Handle(ChangeTaskQuery request, CancellationToken cancellationToken)
     {
