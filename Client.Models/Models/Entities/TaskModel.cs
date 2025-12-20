414fa7fec3b27f3f313e7aa0c04ee2f7187e0bd6
Lychee <2006tka@gmail.com>
2025-12-20 19:55:42 +0500

    абоба

--- Client.Models/Models/Entities/TaskModel.cs
+++ Client.Models/Models/Entities/TaskModel.cs
@@ -10,6 +10,6 @@ public class TaskModel
     public TaskModelExtendedInfo ExtendedInfo { get; set; }
     
     
-    private const double Coefficient = 0.2;
+    private const double Coefficient = 0.5;
     public int Swap => (int)(Reward * Coefficient);
 }
